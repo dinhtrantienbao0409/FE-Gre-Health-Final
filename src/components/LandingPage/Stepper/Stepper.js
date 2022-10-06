079d379a14497ef87d2a332611db0a9d0ae5f76b
tienbao <dinhtrantienbao0409@gmail.com>
2022-10-07 03:41:57 +0700

    register stepper form final

--- src/components/LandingPage/Stepper/Stepper.js
+++ src/components/LandingPage/Stepper/Stepper.js
@@ -1,105 +1,171 @@
-import { useEffect, useState, useRef } from "react";
+import React, { useState } from "react";
+import { useFormContext } from "react-hook-form";
+import _ from "lodash";
+import { useNavigate } from "react-router-dom";
 
-export default function Stepper({ steps, currentStep }) {
-  const [newStep, setNewStep] = useState([]);
-  const stepRef = useRef();
-  const updateStep = (stepNumber, steps) => {
-    const newSteps = [...steps];
-    let count = 0;
-    while (count < newSteps.length) {
-      if (count === stepNumber) {
-        newSteps[count] = {
-          ...newSteps[count],
-          completed: true,
-          highlighted: true,
-          selected: true,
-        };
-        count++;
-      } else if (count < stepNumber) {
-        newSteps[count] = {
-          ...newSteps[count],
-          completed: true,
-          highlighted: true,
-          selected: true,
-        };
-        count++;
-      } else {
-        newSteps[count] = {
-          ...newSteps[count],
-          completed: false,
-          highlighted: false,
-          selected: false,
-        };
-        count++;
+import { Stepper, Step, StepLabel, Button } from "@mui/material";
+
+import { Account } from "components/LandingPage/Stepper/Steps/Account";
+import { Details } from "components/LandingPage/Stepper/Steps/Details";
+
+import { registerFunc } from "services/Auth";
+
+export const FormStepper = () => {
+  const navigate = useNavigate();
+  const { watch, errors } = useFormContext();
+  const [activeStep, setActiveStep] = useState(0);
+  const [compiledForm, setCompiledForm] = useState({});
+  console.log(
+    "🚀 ~ file: Stepper.js ~ line 143 ~ FormStepper ~ compiledForm",
+    compiledForm
+  );
+
+  function getSteps() {
+    return ["Account", "Details"];
+  }
+
+  function getStepContent(step, formContent) {
+    switch (step) {
+      case 0:
+        return <Account {...{ formContent }} />;
+      case 1:
+        return <Details {...{ formContent }} />;
+      default:
+        return "Unknown step";
+    }
+  }
+
+  const steps = getSteps();
+  const form = watch();
+
+  console.log("🚀 ~ file: Stepper.js ~ line 149 ~ FormStepper ~ form", form);
+
+  const handleNext = () => {
+    let canContinue = true;
+
+    switch (activeStep) {
+      case 0:
+        setCompiledForm({ ...compiledForm, account: form });
+        canContinue = true;
+        break;
+      case 1:
+        setCompiledForm({ ...compiledForm, details: form });
+        canContinue = handleSubmit({ ...compiledForm, details: form });
+        break;
+
+      default:
+        return "not a valid step";
+    }
+    if (canContinue) {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    }
+  };
+
+  const handleBack = () => {
+    if (activeStep > 0) {
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+      switch (activeStep) {
+        case 1:
+          setCompiledForm({ ...compiledForm, details: form });
+          break;
+        default:
+          return "not a valid step";
       }
     }
-    return newSteps;
   };
-  useEffect(() => {
-    const stepsState = steps.map((step, index) =>
-      Object.assign(
-        {},
-        {
-          description: step,
-          completed: false,
-          highlighted: index === 0 ? true : false,
-          selected: index === 0 ? true : false,
-        }
-      )
-    );
-    stepRef.current = stepsState;
-    const current = updateStep(currentStep - 1, stepRef.current);
-    setNewStep(current);
-  }, [steps, currentStep]);
 
-  const displaySteps = newStep.map((step, index) => {
-    return (
-      <div
-        key={index}
-        className={
-          index !== newStep.length - 1
-            ? " w-full flex items-center "
-            : "flex items-center"
-        }
-      >
-        <div className="relative flex flex-col items-center text-gray-800">
-          <div
-            className={`rounded-full transition duration-500 border-2 border-gray-300 h-12 w-12 flex items-center justify-center py-3 ${
-              step.selected
-                ? "bg-green-600 text-white font-bold border border-green-600"
-                : ""
-            }`}
-          >
-            {/*number */}
-            {step.completed ? (
-              <span className=" text-white font-bold text-xl">&#10003;</span>
-            ) : (
-              index + 1
-            )}
-          </div>
-          <div
-            className={`absolute top-0 text-center mt-16 w-32 text-xs font-medium uppercase ${
-              step.selected ? "text-green-600" : "text-gray-400"
-            }`}
-          >
-            {/*description */}
-            {step.description}
-          </div>
-        </div>
-        <div
-          className={`flex-auto border-t-2 transition duration-500 ${
-            step.completed ? "border-green-600" : "border-gray-300"
-          }`}
-        >
-          {/*line */}
-        </div>
-      </div>
-    );
-  });
+  const handleReset = () => {
+    navigate("/login");
+  };
+
+  const handleSubmit = async (form) => {
+    if (_.isEmpty(errors)) {
+      console.log("submit", form);
+
+      const accountData = form.account;
+      console.log(
+        "🚀 ~ file: Stepper.js ~ line 198 ~ handleSubmit ~ accountData",
+        accountData
+      );
+      const userData = form.details;
+      console.log(
+        "🚀 ~ file: Stepper.js ~ line 200 ~ handleSubmit ~ userData",
+        userData
+      );
+
+      const payload = Object.assign(accountData, userData);
+      console.log(
+        "🚀 ~ file: Stepper.js ~ line 225 ~ handleSubmit ~ payload",
+        payload
+      );
+
+      const response = await registerFunc(payload);
+
+      const token = response.data;
+      if (!token) return;
+      localStorage.setItem("access_token", token);
+    }
+  };
 
   return (
-    <div className="mx-4 p-4 flex justify-between items-center">
-      {displaySteps}
+    <div>
+      <div>
+        <h2 className="my-6 text-center text-3xl font-extrabold text-gray-900">
+          Register Form
+        </h2>
+      </div>
+      <Stepper className="mx-32" activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps = {};
+          const labelProps = {};
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <div className=" mt-4 flex flex-col items-center gap-5">
+              <div className="text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-20 h-20"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="mt-3 text-xl font-semibold uppercase text-blue-500">
+                Congratulations!
+              </div>
+              <div className="text-lg font-semibold text-gray-400">
+                Your Account has been created.
+              </div>
+              <Button variant="contained" onClick={() => handleReset()}>
+                Login
+              </Button>
+            </div>
+          </div>
+        ) : (
+          <div>
+            {getStepContent(activeStep, compiledForm)}
+            <div>
+              <Button onClick={handleBack}>Back</Button>
+              <Button variant="contained" color="primary" onClick={handleNext}>
+                {activeStep === steps.length - 1 ? "Finish" : "Next"}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
--- src/pages/Register/RegisterPage.js
+++ src/pages/Register/RegisterPage.js
@@ -1,195 +1,46 @@
-// import { useForm } from "react-hook-form";
-import { registerFunc } from "services/Auth";
-import { useNavigate } from "react-router-dom";
-// import { yupResolver } from "@hookform/resolvers/yup";
+import React, { useEffect } from "react";
+import { FormStepper } from "components/LandingPage/Stepper/Stepper";
+import { useForm, FormProvider } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { useState } from "react";
-import Stepper from "components/LandingPage/Stepper/Stepper";
-import StepperControl from "components/LandingPage/Stepper/StepperControl";
-import Details from "components/LandingPage/Stepper/Steps/Details";
-import Final from "components/LandingPage/Stepper/Steps/Final";
-import Account from "components/LandingPage/Stepper/Steps/Account";
-import { useSelector } from "react-redux";
 
-// const schemaValidation = yup.object().shape({
-//   email: yup.string().email().required(),
-//   password: yup.string().required().min(3),
-//   confirmPassword: yup
-//     .string()
-//     .oneOf([yup.ref("password"), null], "Passwords must match"),
-// });
+const schemaValidation = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().required().min(3),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match"),
+  name: yup.string().required(),
+  address: yup.string().required(),
+  dateOfBirth: yup.string().required(),
+  gender: yup.string().required(),
+  jobTitle: yup.string().required(),
+  contact: yup.string().required(),
+});
 
 const Register = () => {
+  const methods = useForm({
+    mode: "onTouched",
+    resolver: yupResolver(schemaValidation),
+  });
+
   const {
-    email,
-    password,
-    confirmPassword,
-    name,
-    dateOfBirth,
-    gender,
-    address,
-    contact,
-    jobTitle,
-  } = useSelector((state) => state.user);
-  const navigate = useNavigate();
-  const [error, setError] = useState("");
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm({ resolver: yupResolver(schemaValidation) });
+    watch,
+    formState: { errors },
+  } = methods;
 
-  /* Stepper */
-  const [currentStep, setCurrentStep] = useState(1);
-  const steps = ["Account Information", "Personal Details", "Complete"];
-  const displayStep = (step) => {
-    switch (step) {
-      case 1:
-        return <Account />;
-      case 2:
-        return <Details />;
-      case 3:
-        return <Final />;
-      default:
-        break;
-    }
-  };
-
-  const handleClick = (direction) => {
-    let newStep = currentStep;
-    direction === "next" ? newStep++ : newStep--;
-    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
-  };
-  /* Stepper */
-
-  const handleRegister = async () => {
-    try {
-      const payload = {
-        email,
-        password,
-        confirmPassword,
-        name,
-        dateOfBirth,
-        gender,
-        address,
-        contact,
-        jobTitle,
-      };
-
-      const response = await registerFunc(payload);
-
-      const token = response.data;
-      if (!token) return;
-      localStorage.setItem("access_token", token);
-      navigate("/login");
-    } catch (error) {
-      setError(error.message);
-    }
-  };
+  useEffect(() => {
+    console.log("FORM CONTEXT", watch(), errors);
+  }, [watch, errors]);
 
   return (
     <div className="md:w-1/2 mx-auto my-48 shadow-xl rounded-2xl pb-2 bg-white">
       <div className="container horizontal my-10">
-        <Stepper steps={steps} currentStep={currentStep} />
-
-        {/*contexts */}
-        <div className="my-10 mx-10">{displayStep(currentStep)}</div>
+        <FormProvider {...methods}>
+          <FormStepper />
+        </FormProvider>
       </div>
-      <StepperControl
-        handleClick={handleClick}
-        currentStep={currentStep}
-        steps={steps}
-      />
     </div>
-    // <div className="min-h-full flex items-center justify-center py-48 px-4 sm:px-6 lg:px-8">
-    //   <div className=" p-10 shadow-xl max-w-md  w-full space-y-8 gap-5 rounded-lg">
-    //     <div>
-    //       <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-    //         Create your account
-    //       </h2>
-    //     </div>
-    //     <form
-    //       className="mt-8 space-y-6"
-    //       onSubmit={handleSubmit(handleRegister)}
-    //     >
-    //       <input type="hidden" name="remember" defaultValue="true" />
-    //       <div className="rounded-md shadow-sm -space-y-px">
-    //         <div>
-    //           <label htmlFor="email-address" className="sr-only">
-    //             Email address
-    //           </label>
-    //           <input
-    //             id="email-address"
-    //             name="email"
-    //             type="text"
-    //             autoComplete="email"
-    //             className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-    //             placeholder="Email address"
-    //             {...register("email", { required: true })}
-    //           />
-    //         </div>
-    //         <div>
-    //           {errors.email && (
-    //             <span className="text-sm text-red-600">
-    //               {errors?.email?.message}
-    //             </span>
-    //           )}
-    //         </div>
-    //         <div>
-    //           <label htmlFor="password" className="sr-only">
-    //             Password
-    //           </label>
-    //           <input
-    //             id="password"
-    //             name="password"
-    //             type="password"
-    //             autoComplete="current-password"
-    //             className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-    //             placeholder="Password"
-    //             {...register("password", { required: true })}
-    //           />
-    //         </div>
-    //         <div>
-    //           {errors.password && (
-    //             <span className="text-sm text-red-600">
-    //               {errors?.password?.message}
-    //             </span>
-    //           )}
-    //         </div>
-    //         <div>
-    //           <label htmlFor="password" className="sr-only">
-    //             Confirm Password
-    //           </label>
-    //           <input
-    //             id="confirmPassword"
-    //             name="confirmPassword"
-    //             type="password"
-    //             autoComplete="current-password"
-    //             className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-    //             placeholder="Confirm Password"
-    //             {...register("confirmPassword", { required: true })}
-    //           />
-    //         </div>
-    //         <div>
-    //           {errors.confirmPassword && (
-    //             <span className="text-sm text-red-600">
-    //               {errors?.confirmPassword?.message}
-    //             </span>
-    //           )}
-    //         </div>
-    //       </div>
-
-    //       <div>
-    //         <button
-    //           type="submit"
-    //           className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-cyan-500 hover:bg-cyan-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400"
-    //         >
-    //           Register
-    //         </button>
-    //       </div>
-    //     </form>
-    //   </div>
-    // </div>
   );
 };
 
